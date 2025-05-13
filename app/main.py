b2d99d496a24e054ff0e66e3f9620822fb904d09
ahngj <97014388+ahngj@users.noreply.github.com>
2025-05-13 17:11:01 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,6 +7,14 @@ app = FastAPI(
     version="0.1.0"
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # 라우터 등록
 app.include_router(predict_router)
 app.include_router(session_router)
