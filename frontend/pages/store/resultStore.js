dcc87144ab49b54947fd6b230fbc8812520493e8
ahngj <97014388+ahngj@users.noreply.github.com>
2025-05-26 13:43:05 +0900

    Delete frontend/pages directory

--- frontend/pages/store/resultStore.js
+++
@@ -1,9 +0,0 @@
-// store/resultStore.js
-import { create } from 'zustand';
-
-const useResultStore = create((set) => ({
-  results: {},
-  setResults: (data) => set({ results: data }),
-}));
-
-export default useResultStore;
